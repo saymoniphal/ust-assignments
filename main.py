5b0a20be07f244947db0048944baa906d451aedf
Moniphal Say <moniphal@localhost.localdomain>
2024-01-24 11:15:48 -0800

    read input from file and calls all function in src with data from file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import concurrent.futures
 import argparse
 
+from typing import List
 from src import word_frequency, check_pwd, circular_queue
 
 def parse_args():
@@ -9,20 +10,48 @@ def parse_args():
                     description='Call multiple scripts based on arguments',)
     
     parser.add_argument('--filename', dest='filename', type=str)
-    args = parser.parse_args()
+    return parser.parse_args()
 
-def cq():
+def get_file_content(filename: str) -> List[str]:
+    """
+    Read data from file.
+    returns: list of string of each line from file.
+    """
+    with open(filename, '+r') as fin:
+        data = fin.readlines()
+    return data
+
+def run_func(filename, apply_func) -> any:
+    """
+    A helper function to read data from filename and feed to function apply_func
+    """
+    data = get_file_content(filename)
+    for line in data:
+        print(f'Running function: {apply_func.__name__}')
+        apply_func(line.strip())
+
+def __cq__(value: str) -> None:
+    # helper function to enqueue and display queue to use in multi-thread
     cq = circular_queue.CircularQueue()
-    cq.enqueue('a')
-    cq.enqueue('b')
+    cq.enqueue(value)
+    cq.enqueue(value)
     cq.displayQueue()
 
-def main():
-    # run scripts in src in parallel
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as exec:
-        exec.submit(word_frequency.print_words_freq('test is test'))
-        exec.submit(check_pwd.print_pwds('asAqwr1234@1,aF145#,2w3E*,@Favb#,9zMX*'))
-        exec.submit(cq())
+    cq.dequeue()
+    cq.displayQueue()
+
+def main() -> None:
+    options = parse_args()
+    if options.filename: # read input from file
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as exec:
+            exec.submit(run_func(options.filename, word_frequency.print_words_freq))
+            exec.submit(run_func(options.filename, check_pwd.print_pwds))
+            exec.submit(run_func(options.filename, __cq__))
+    else: # run scripts in src in parallel with some sample inputs
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as exec:
+            exec.submit(word_frequency.print_words_freq('This is a test test'))
+            exec.submit(check_pwd.print_pwds('asAqwr1234@1,aF145#,2w3E*,@Favb#,9zMX*'))
+            exec.submit(__cq__('a'))
 
 if __name__ == '__main__':
     main()
